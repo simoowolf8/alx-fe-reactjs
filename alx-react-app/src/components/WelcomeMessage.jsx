cdb3eb3cc764d8f9720883098c507b15bfc975b1
simodevjs <simodevjs@gmail.com>
2024-11-18 05:17:45 +0100

    alx_ub3

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -2,7 +2,7 @@ function WelcomeMessage() {
     return (
         <div>
             <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
         </div>
     );
 }
