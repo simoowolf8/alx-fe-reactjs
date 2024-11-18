ca70c7596d31aeed0a20af01112592598bb60d93
simodevjs <simodevjs@gmail.com>
2024-11-18 05:40:27 +0100

    alx_ub3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +13,7 @@ function App() {
   return (
     <>
       <div>
+	<Header />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,6 +21,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <MainContent />
       <h1>Hello everyone, I am learning React at ALX!</h1>
       <p>I am learning about JSX!</p>
       <WelcomeMessage />
@@ -31,6 +36,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
   )
 }
