a1f67c087204b7c0d7862a285da10783c6ea3513
simodevjs <simodevjs@gmail.com>
2024-11-27 05:18:51 +0100

    alx_ub3

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile.jsx'
+import ProfilePage from './components/ProfilePage.jsx'
+import UserContext from './UserContext.js';
+
+function App() {
+  const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <>
+      <div>
+	<Header />
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <MainContent />
+      <UserContext.Provider value={userData}>
+        <ProfilePage userData={userData}/>
+      </UserContext.Provider>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>I am learning about JSX!</p>
+      <WelcomeMessage />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react';
+
+// Create the context
+const UserContext = createContext();
+
+export default UserContext;
