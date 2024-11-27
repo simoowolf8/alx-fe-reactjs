ba9c42531aeb73278b892f67d228352a2795824e
simodevjs <simodevjs@gmail.com>
2024-11-27 04:15:15 +0100

    alx_ub3

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
