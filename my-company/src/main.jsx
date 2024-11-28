ca02ca933de715e6b1375caf4c1e0086f90c5db3
simodevjs <simodevjs@gmail.com>
2024-11-28 03:57:35 +0100

    alx_ub3

---
+++ my-company/src/main.jsx
@@ -0,0 +1,16 @@
+import structuredClone from 'structured-clone';
+
+if (typeof globalThis.structuredClone !== 'function') {
+  globalThis.structuredClone = structuredClone;
+}
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
