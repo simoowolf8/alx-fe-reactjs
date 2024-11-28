d657d718141d973d71edcbb514bb8503cb2dcb4b
simodevjs <simodevjs@gmail.com>
2024-11-28 03:00:15 +0100

    dzdd

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -3,4 +3,8 @@ import React, { createContext } from 'react';
 // Create the context
 const UserContext = createContext();
 
+
+/** @constant {React.Context} UserContext - The context for user data.
+    */
+
 export default UserContext;
