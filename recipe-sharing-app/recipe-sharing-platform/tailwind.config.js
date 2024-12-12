66ad3aef7f626ef4c1f424b14b60c88986e2124b
simodevjs <simodevjs@gmail.com>
2024-12-12 12:17:14 +0100

    ddedededed

--- recipe-sharing-app/recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-app/recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,9 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // You can set it to 'media' or 'class' if needed
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
