235cee37f9df34257688b0bdfaeca8d23ccbfa51
simodevjs <simodevjs@gmail.com>
2024-12-14 03:06:05 +0100

    e"e"e

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RecipeDetail from './components/RecipeDetail';
+import HomePage from './components/HomePage'; // Import HomePage component
+import RecipeDetail from './components/RecipeDetail'; // Import RecipeDetail component
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Home Page */}
+        {/* Home Page Route */}
         <Route path="/" element={<HomePage />} />
-        
-        {/* Recipe Detail Page */}
+
+        {/* Recipe Detail Route */}
         <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
